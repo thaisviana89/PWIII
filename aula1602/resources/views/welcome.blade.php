518f1a52d1d48a41fb95ba6b239832193b136ed1
thaisviana89 <thaisviana89@gmail.com>
2023-02-16 22:29:32 -0300

    primeiro proejto em laravel

---
+++ aula1602/resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li> <a href="">Welcome</a></li>
+            <li> <a href="/contato">Contato</a></li>
+            <li> <a href="/quemsomos">Quem somos</a></li>
+        </ul>
+
+        <h1>Como iniciar um projeto em laravel</h1>
+
+        <p>
+        1 - Abrir o CMD e voltar para a raiz (cd \);
+        </br>
+        </br>
+        2 - Criar um diretorio chamado laravel (mkdir laravel);
+        </br>
+        </br>
+        3 - Verificar se o composer está instalado (composer -v);
+        </br>
+        </br>
+        4 - Executar o comando (composer create-project laravel/laravel <nome do projeto> --prefer-dist) no lugar de aula1 colocar o nome do seu projeto
+        </br>
+        </br>
+        5 - Vai executar uns comandos de instalação automático, então vai parar na seguinte questão: 
+        </br>
+        Do you trust "kylekatarnls/update-helper" to execute code and wish to enable it now? (writes "allow-plugins" to composer.json) [y,n,d,?]  colocar y e dar enter     
+        </br>
+        </br>
+        6 - Entrar na pasta que criou (cd <nome da pasta>)
+        </br>
+        </br>
+        7 - Iniciar o code (code .)     
+        </br>
+        </br>
+        8 - Iniciar o laravel com (php artisan serve)
+
+        </p>
+    </nav>
+</body>
+</html>
---
+++ aula1602/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+// get('nome que irá aparecer na url')
+Route::get('/quemsomos', function () {
+    //view ('nome do arquivo que está em resource/view')
+    return view('quem-somos');
+});
